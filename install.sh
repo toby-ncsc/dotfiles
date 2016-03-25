40bd4c872efbd68a45b8047c8238bdbe684d5608
Toby Wilkins <tobwilk@gmail.com>
2016-03-25 14:46:18 +0000

    updated

--- install.sh
+++ install.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
+#
 # Install Powerline
+#
+
 apt-get install -y python-pip
-pip install git+git://github.com/Lokaltog/powerline
+pip install powerline-status
 
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
 wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
-
 mv PowerlineSymbols.otf /usr/share/fonts/
-
+mv 10-powerline-symbols.conf /etc/fonts/conf.d/
 fc-cache -vf /usr/share/fonts/
 
-mv 10-powerline-symbols.conf /etc/fonts/conf.d/
+#
+# symlink the vim files
+#
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -sf $DIR/.vimrc ~/.vimrc
+ln -sf $DIR/.vim ~/.vim
+
